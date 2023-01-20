8dc11b0b4b89d685638531c49e94a95b4ff71183
sh310310 <110559375+sh310310@users.noreply.github.com>
2023-01-19 17:01:38 -0800

    done

---
+++ FinalProject/main.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      // console.log(index);
+      link.style.animation = `navLinksFade 0.5s ease forwards ${
+        index / 5 + 0.4
+      }s`;
+    }
+  });
+});
